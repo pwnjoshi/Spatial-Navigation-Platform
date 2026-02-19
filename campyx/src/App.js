a71cce4898637859bfeb0268aed56077639fd12f
pwnjoshi <joshipawan2021@gmail.com>
2026-02-19 09:52:17 +0530

    Updated Home page

---
+++ campyx/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="status">
+          Under Development
+        </div>
+        
+        <h1>Spatial Navigation Platform</h1>
+        
+        <p className="team">By Team Campyx</p>
+        
+        <footer>
+          <p>&copy; 2026 Team Campyx</p>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
